9d620b93c39f3238614b4b96a384d9b079869801
cmolinet <cmolinet@users.noreply.github.com>
2026-06-27 12:40:39 +0000

    deploy: 8b29599f22c4a7e29aae3f0ec52745367600ccfb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "767968656" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3868286470" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
